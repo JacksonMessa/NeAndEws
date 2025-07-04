dd35990644709f7fd5f604d7fd17432738710fe7
JacksonMessa <jacksonadrianomessa@gmail.com>
2025-07-04 16:38:33 -0300

    feat: add default button component

---
+++ src/app/components/default-button/default-button.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'default-button',
+  imports: [],
+  templateUrl: './default-button.html',
+  styleUrl: './default-button.scss'
+})
+export class DefaultButton {
+  @Input() text:string = "";
+  @Input() btnClass:string = "normal";
+
+  
+}
