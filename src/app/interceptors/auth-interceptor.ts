72719ff102f3e539172505fac23cd7e6c7b6909e
JacksonMessa <jacksonadrianomessa@gmail.com>
2025-08-12 15:47:15 -0300

    feat: user automatically logs out if the token is invalid or expired

--- src/app/interceptors/auth-interceptor.ts
+++ src/app/interceptors/auth-interceptor.ts
@@ -1,8 +1,14 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { catchError, throwError } from 'rxjs';
+import { UserService } from '../services/user.service';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   
   const token = localStorage.getItem("auth-token");
+  const toastrService = inject(ToastrService);
+  const userService = inject(UserService);
 
     if(token){
       req = req.clone({
@@ -12,5 +18,13 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
       })
     }
 
-    return next(req)
+    return next(req).pipe(
+      catchError(error =>{
+        if(error.status == 401){
+          toastrService.error("Your token has expired or is invalid, please log in again.");
+          userService.logout();
+        }
+        return throwError(() => error);
+      })
+    )
 };
